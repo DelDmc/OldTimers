28a25594568f21ed664071cb955f45a54909ba25
K.S <del12dmc@gmail.com>
2022-06-23 23:50:59 +0300

    HW26 1. Search form works well, but price range disabled (but works if enabled)

--- src/oldtimers/models.py
+++ src/oldtimers/models.py
@@ -1,9 +1,6 @@
 import uuid as uuid
-from decimal import Decimal
 
 from colorfield.fields import ColorField
-from django.contrib.auth import get_user_model
-from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from django.utils.translation import gettext as _
@@ -51,7 +48,7 @@ class Retailer(BaseModel):
         return f"{self.company_name} - {self.city} ({self.get_country_display()})"
 
     def vehicles_count(self):
-        return self.vehicles.count()
+        return self.vehicle.count()
 
     def save(self, *args, **kwargs):
         self.full_clean()
--- src/oldtimers/views.py
+++ src/oldtimers/views.py
@@ -1,16 +1,14 @@
 from decimal import Decimal
-from random import randint, random, shuffle
+from random import shuffle
 
-from django.db.models import Q
-from django.http import Http404, HttpResponse
-# Create your views here.
-from django.shortcuts import render
 from django.views.generic import ListView, TemplateView
 from django.views.generic.base import TemplateResponseMixin
 from webargs import fields
-from webargs.djangoparser import use_args, use_kwargs
+from webargs.djangoparser import use_args
 
 from oldtimers.models import Retailer, Vehicle
+
+# Create your views here.
 
 
 class IndexView(TemplateView):
@@ -50,13 +48,15 @@ class RetailerListView(ListView, TemplateResponseMixin):
     context_object_name = "retailers"
     queryset = Retailer.objects.all()
     additional_context = "retailers_vehicles"
-    search_form_context = "sorted_retailer"
+    search_form_context = "sorted_vehicles"
 
     vehicle_args = {
         "retailer": fields.Str(required=False),
         "brand": fields.Str(required=False),
-        "production_year": fields.Int(required=False,),
-        "price__range": fields.Str(required=False)
+        "production_year": fields.Int(
+            required=False,
+        ),
+        "price__range": fields.Str(required=False),
     }
 
     def get_context_data(self, **kwargs):
@@ -64,31 +64,26 @@ class RetailerListView(ListView, TemplateResponseMixin):
         for retailer in self.queryset:
             vehicles = Vehicle.objects.filter(retailer__company_name__contains=retailer.company_name)
             context[retailer] = [vehicle for vehicle in vehicles]
-
         kwargs["retailers_vehicles"] = context
-        print("kwargs", kwargs)
         return kwargs
 
     @use_args(vehicle_args, location="query")
     def get(self, request, parameters, *args, **kwargs):
-        print("parameters", parameters)
-
         self.object_list = self.get_queryset()
         context = self.get_context_data()
-        vehicles = [vehicle for vehicle in self.get_vehicles(request, parameters)]
-        context["sorted_retailer"] = vehicles
-        print("context", context)
+        vehicles = self.get_vehicles(request, parameters)
+        context["sorted_vehicles"] = vehicles
         return self.render_to_response(context)
 
     def get_vehicles(self, request, parameters):
-        price_range = parameters.get('price__range').split(";")
-        parameters['price__range'] = tuple(float(item) for item in price_range)
-        retailer_sorted_vehicles = [
-            Vehicle.objects.filter(**{param_name: param_value}) for param_name, param_value in parameters.items()
-        ]
-        print("retailer_sorted_vehicles", retailer_sorted_vehicles)
-        print("parameters", parameters)
-        return retailer_sorted_vehicles
+        """Returns list of sorted vehicles as per data received from parameters"""
+        if parameters:
+            search_form_context = None
+            price_range = parameters.get("price__range").split(";")
+            parameters["price__range"] = tuple(Decimal(item) for item in price_range)
+            for param_name, param_value in parameters.items():
+                search_form_context = Vehicle.objects.filter(**{param_name: param_value})
+            return search_form_context
 
 
 class ContactsView(TemplateView):
